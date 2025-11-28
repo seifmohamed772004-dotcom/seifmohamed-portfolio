9d68b173ae0b3cb5acdd3a66b314903a65d8e358
Seif Mohamed <seifmohamed772004@gmail.com>
2025-11-28 20:28:36 +0200

    Adjustments2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Sec1Home from '../Common-HOME/Sec1.jsx';
 import './Home.css';
 import StaticPropsMarquee from '../Common-HOME/Marquee.jsx'
 import Sec2Home from '../Common-HOME/Sec2.jsx'
+import Sec3Home from '../Common-HOME/Sec3.jsx'
 import Sec4Home from '../Common-HOME/Sec4.jsx'
 import Sec5Home from '../Common-HOME/Sec5.jsx'
 import Sec6Home from '../Common-HOME/Sec6.jsx'
@@ -18,7 +19,7 @@ const Home = () => {
       <Sec1Home />
       <StaticPropsMarquee />
       <Sec2Home/>
-      {/* Sec 3 Lesa mat3amaletsh dy btaaet el featured project fa e3melha w add makan el comment */}
+<Sec3Home/>
       <Sec4Home/>
       <Sec5Home/>
       <Sec6Home/>
