3e8861f8e26e083083d5f8d947ba50751b3360a9
Seif Mohamed <seifmohamed772004@gmail.com>
2025-12-20 19:51:33 +0200

    FEAT Added all sections to the about page (Missing Importing to router and Linking to Nav)

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Nav from '../Components/Nav';
 import Sec4Home from '../Common-HOME/Sec4.jsx'
 import StaticPropsMarquee from '../Common-HOME/Marquee.jsx'
-
+import Sec2Home from '../Common-HOME/Sec2.jsx'
+import Sec5Home from '../Common-HOME/Sec5.jsx'
 
 import Footer from '../Components/Footer';
 const About = () => {
@@ -10,6 +11,8 @@ const About = () => {
         <Nav/>
 <Sec4Home/>
         <StaticPropsMarquee/>
+        <Sec2Home/>
+<Sec5Home/>
         <Footer/>
     </> );
 }
