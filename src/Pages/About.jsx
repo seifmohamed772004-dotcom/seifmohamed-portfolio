0cc47cd6ae5517404aeb6feeb36bf809baca49d1
Seif Mohamed <seifmohamed772004@gmail.com>
2025-12-20 20:56:25 +0200

    FEAT Created Blog Page (Missing Sections / Some Styling and linking to Nav)

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from '../Components/Nav';
-import Sec4Home from '../Common-HOME/Sec4.jsx'
+import Sec1About from '../Common-About/Sec1About.jsx'
 import StaticPropsMarquee from '../Common-HOME/Marquee.jsx'
 import Sec2Home from '../Common-HOME/Sec2.jsx'
 import Sec5Home from '../Common-HOME/Sec5.jsx'
@@ -9,7 +9,9 @@ import Footer from '../Components/Footer';
 const About = () => {
     return ( <>
         <Nav/>
-<Sec4Home/>
+        <section className='holdingSec'>
+<Sec1About/>
+</section>
         <StaticPropsMarquee/>
         <Sec2Home/>
 <Sec5Home/>
---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NAV from '../Components/Nav';
+import Footer from '../Components/Footer';
+const Blog = () => {
+    return ( <>
+<NAV/>
+<Footer/>
+    </> );
+}
+ 
+export default Blog;
