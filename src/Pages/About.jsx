16995649786fb66127f3fbd5fe8109556427e105
Seif Mohamed <seifmohamed772004@gmail.com>
2025-12-20 19:49:55 +0200

    FEAT Added the first section and marquee to the About Page (Missing Sections and Linking)

---
+++ src/Pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../Components/Nav';
+import Sec4Home from '../Common-HOME/Sec4.jsx'
+import StaticPropsMarquee from '../Common-HOME/Marquee.jsx'
+
+
+import Footer from '../Components/Footer';
+const About = () => {
+    return ( <>
+        <Nav/>
+<Sec4Home/>
+        <StaticPropsMarquee/>
+        <Footer/>
+    </> );
+}
+ 
+export default About;
